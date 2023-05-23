84bc906195077946064ad1424a9d5b0fbb8c2931
jkeefe77 <jvkeefe@gmail.com>
2023-05-23 15:27:14 -0400

    JS updates
    added Pokemon API to loadList

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20'
 
   function add(pokemon) {
     if (
